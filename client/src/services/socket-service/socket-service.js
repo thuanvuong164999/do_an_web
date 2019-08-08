a5ff70461e1f0d877822ee7b946df08144afe40e
kimson <anhyeucay1987@gmail.com>
2019-08-08 16:09:52 +0700

    head roomlist

--- client/src/services/socket-service/socket-service.js
+++ client/src/services/socket-service/socket-service.js
@@ -1,7 +1,7 @@
 import socketIOClient from 'socket.io-client'
 
 //khai báo các biến không đổi và sử dụng thường xuyên
-export const serverEndPoint = 'http://cd4ebab3.ngrok.io'
+export const serverEndPoint = 'http://4227a8ab.ngrok.io'
 
 export const socket = socketIOClient(serverEndPoint)
 export const userName = 'Kim Son'
