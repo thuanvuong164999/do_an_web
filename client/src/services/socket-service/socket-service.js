29b1726628a7ed82cbf5e09c4d9fc962790ab621
kimson <anhyeucay1987@gmail.com>
2019-08-19 13:20:29 +0700

    xcvnb

--- client/src/services/socket-service/socket-service.js
+++ client/src/services/socket-service/socket-service.js
@@ -1,7 +1,7 @@
 import socketIOClient from 'socket.io-client'
 
 //khai báo các biến không đổi và sử dụng thường xuyên
-export const serverEndPoint = 'http://c6561314.ngrok.io kimson'
+export const serverEndPoint = 'http://c6561314.ngrok.io'
 
 export const socket = socketIOClient(serverEndPoint)
 export const userName = 'Kim Son'
