e4347dd8ee2a3e5e41f1ca64cced99117b5b3adc
Vuong Dung Thuan <dungthuan2468@gmail.com>
2019-08-22 17:50:54 +0700

    new

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -112,9 +112,16 @@ class LoginPages extends React.Component {
                     </ButtonToolbar>  */}
                     {/* <a href='/chat'><button className="login-form-control login-form-button" type='submit' value='LOGIN'>LOGIN</button></a> */} 
                     <a className="login-form-link" href="/chat">LOST YOUR PASSWORD ?</a>
-                    
-                    <div className='icon-face'>
-                        <i class="fab fa-facebook"></i>
+                    <div>
+                        <div className='icon-face'>
+                            <i class="fab fa-facebook"></i>
+                        </div>
+                        <div className='icon-gg'>
+                            <i class="fab fa-google-plus"></i>
+                        </div>
+                        <div className='icon-ins'>
+                            <a><i class="fab fa-instagram"></i></a>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
