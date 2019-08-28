e0ebc98995eaa0567eaf67a9478c009cd46fb9e8
kimson <anhyeucay1987@gmail.com>
2019-08-28 13:10:11 +0700

    delete

--- client/src/services/socket-service/socket-service.js
+++ client/src/services/socket-service/socket-service.js
@@ -4,11 +4,11 @@ import socketIOClient from 'socket.io-client'
 export const serverEndPoint = 'http://679319b2.ngrok.io'
 
 export const socket = socketIOClient(serverEndPoint)
-export const userName = 'vuong dung thuan'
+// export const userName = 'vuong dung thuan'
 export const userId = '4'
 // export const userName = 'minh hoang'
 // export const userId = '6'
-// export const userName = 'kim son'
+export const userName = 'kim son'
 // export const userId = '5'
 
 
