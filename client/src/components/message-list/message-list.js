c4f232ba1a6f50d2aff83295314b901c7c72383e
Vuong Dung Thuan <dungthuan2468@gmail.com>
2019-10-14 13:20:28 +0700

    fix

--- client/src/components/message-list/message-list.js
+++ client/src/components/message-list/message-list.js
@@ -62,6 +62,8 @@ class MessageList extends React.Component {
             let items = []
             values.rows.map((value, index) => {
                 // console.log(value)
+                // console.log(value.user_id, cookie.get('loginId'))
+                // console.log(value.user_id == cookie.get('loginId'))
                 // let user = value.username.trim()
                 let user = value.username
                 let item = {
@@ -70,7 +72,7 @@ class MessageList extends React.Component {
                     message: value.message,
                     dat: value.datime,
                     daday:value.daday,
-                    fr: user === cookie.get('logined') ? 'fr' : '',
+                    fr: value.user_id == cookie.get('loginId') ? 'fr' : '', // 2 dau =, so sanh gia tri khac loai
                 }
                 items.push(item)
             })
