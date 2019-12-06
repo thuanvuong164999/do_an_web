be6cd6a1e819bfdc339e43779c79d34b18eca2b2
kimson <anhyeucay1987@gmail.com>
2019-12-06 20:53:18 +0700

    chat

--- client/src/services/socket-service/socket-service.js
+++ client/src/services/socket-service/socket-service.js
@@ -1,10 +1,6 @@
 import socketIOClient from 'socket.io-client'
 
 //khai báo các biến không đổi và sử dụng thường xuyên
-<<<<<<< HEAD
-export const serverEndPoint = 'http://2b32f6ba.ngrok.io'
-=======
-export const serverEndPoint = 'http://ee021b7b.ngrok.io'
->>>>>>> 4a65c1a5eb3af7157d941f6d35a8d923ca5ff485
+export const serverEndPoint = 'http://aceb57c6.ngrok.io'
 
 export const socket = socketIOClient(serverEndPoint)
